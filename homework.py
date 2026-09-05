137b0e67b81a416a78814d419976dff5c01e2305
stevengao-ytc <stevengao213@gmail.com>
2026-09-05 13:59:22 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,16 +1,31 @@
-# In class, we have made a simple turtle program that draws a square.
-# The python file (draw_square.py) for this program is included in the repository for your reference.
-# So the homework is to make a triangle in python turtle module, using the functions we learned in class.
+# In class, we have made two functions: find_largest and find_smallest. Using the same idea/code structure, write a function "find_sum" that finds the sum of a number list.
+# The two functions "find_largest" and "find_smallest" are here for your information. You don't need these two functions to write the "find_sum" function.
 
-# TODO: edit this file to make a triangle of any colour or size you like
-# Since we use a new module called turtle, regular python compiler websites (like online-python.com) will not work.
-# Please test your script in turtle supported online python environments, like
-# "https://pythonsandbox.com/turtle"
-# or "https://trinket.io/turtle"
-# or "https://stepindev.com/en/py-playground"
+int_list = [3, 4, 9, 2]
+
+def find_largest(ls):  # ls means list
+    largest = ls[0]
+    for i in ls:
+        if i > largest:
+            largest = i
+    return largest
+
+def find_smallest(ls):
+    smallest = ls[0]
+    for i in ls:
+        if i < smallest:
+            smallest = i
+    return smallest
+
+def find_sum(ls):
+    # TODO
+
+print("smallest = " + find_smallest(int_list))
+print("largest = " + find_largest(int_list))
+print("sum = " + find_sum(int_list))
 
 
-import turtle
+
 
 
 
